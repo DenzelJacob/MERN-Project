4212e5b2849fcb097a1cd6233a84d69e45a7fb4f
Denzel Jacob <70809285+DenzelJacob@users.noreply.github.com>
2022-10-06 10:42:09 -0400

    added useState hooks for create and temp json db

--- workout-app/src/Create.js
+++ workout-app/src/Create.js
@@ -1,13 +1,21 @@
-
-const handleSubmit = (e) =>{
-        console.log(e.target);
+import { useState } from "react";
+import {useHistory} from "react-router-dom";
+const Create = () => {
+    const [name, setName] = useState('');
+    const [sets, setSets] = useState('');
+    const [reps, setReps] = useState('');
+    const [weight, setweight] = useState('');
+    const handleSubmit = (e) =>{
         e.preventDefault()
+        const exercise ={name,sets,reps,weight};
+
+        //fetch(){}
+        console.log(exercise);
+
+
 
     }
-const Create = () => {
-
     
-
 
 
     return (  
@@ -18,17 +26,17 @@ const Create = () => {
 
                     <form className="form">
                     <label>Name of Exercise:</label>
-                    <input type="text" name="name" autoComplete="off"></input>
+                    <input type="text" name="name" autoComplete="off" onChange={(e) => setName(e.target.value)}></input>
                     <br/>
                     
                     <label htmlFor="sets">Sets: </label>
-                    <input type="number" id="sets" placeholder="1"></input>
+                    <input type="number" id="sets" placeholder="0" onChange={(e) => setSets(e.target.value)}></input>
                     <br/>
                     <label htmlFor="reps">Repetitions:</label>
-                    <input type="number" id="reps" placeholder="1"></input>
+                    <input type="number" id="reps" placeholder="0" onChange={(e) => setReps(e.target.value)}></input>
                     <br/>
                     <label htmlFor="weight">Weight:</label>
-                    <input type="number" id="weight" placeholder="0"></input>
+                    <input type="number" id="weight" placeholder="0" onChange={(e) => setweight(e.target.value)}></input>
                     <br/>
 
                     <button className="submitbutton" onClick={handleSubmit}>Submit</button>
