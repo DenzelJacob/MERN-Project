fbd78d0a7135ce454d5e2f93894215b5a9adee1c
Denzel Jacob <70809285+DenzelJacob@users.noreply.github.com>
2022-10-06 11:27:44 -0400

    Cleaning up

--- workout-app/src/Home.js
+++ workout-app/src/Home.js
@@ -1,10 +1,10 @@
 const Home = () => {
     return (  
         <div>
-            
+
         <h2>Welcome to your homepage</h2>
         <h2>Recent Workouts</h2>
-        
+        {/* add script to pull entries from db.json */}
 
         </div>
     );
